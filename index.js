2bac124b08f08a2fe15e9bd6f82284d5b0b8b493
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-10 06:17:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+destructivelyAppendKitten(name) {
+  array.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  array.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  array.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  array.shift()
+}
+
+function appendKitten(name) {
+  return [...array, name]
+}
+
+function prependKitten(name) {
+  return [name, ...array]
+}
+
+function removeLastKitten() {
+  return array.slice(0, array.length -1)
+}
+
+function removeFirstKitten() {
+  return array.slice(1)
+}
